e1c7caf34e7777f4c1e7d402753058488d777c30
Adam Zeloof <azzeloof@gmail.com>
2024-04-28 18:08:52 -0400

    initial commit

---
+++ src/prusacam.py
@@ -0,0 +1,53 @@
+import time
+import requests
+import secret
+from io import BytesIO
+from picamera2 import Picamera2
+
+import httpx
+
+class API:
+    def __init__(self, token, fingerprint):
+        self.token = token
+        self.fingerprint = fingerprint
+        self.url = "https://webcam.connect.prusa3d.com"
+
+    def uploadSnapshot(self):
+        url = self.url + "/c/snapshot"
+        headers = {
+            'accept': '*/*',
+            'Content-Type': 'image/jpg',
+            'fingerprint': self.fingerprint,
+            'token': self.token
+        }
+        response = None
+        with open("current.jpg", 'rb') as f:
+            image = f.read()
+            response = requests.put(url, headers=headers, data=image, stream=True, verify=False)
+        return response
+
+
+class Camera:
+    def __init__(self):
+        self.cam = Picamera2()
+        self.config = self.cam.create_still_configuration(main={"size": (6000, 4000)})
+        self.cam.configure(self.config)
+
+        self.cam.start()
+
+    def snap(self):
+        self.cam.capture_file('current.jpg')
+        
+    def close(self):
+        self.cam.close()
+
+
+if __name__=="__main__":
+    camera = Camera()
+    api = API(secret.TOKEN, secret.FINGERPRINT)
+    time.sleep(2)
+    camera.snap()
+    time.sleep(1)
+    print(api.uploadSnapshot().text)
+    time.sleep(2)
+    camera.close()
